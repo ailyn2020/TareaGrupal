e0f51645d0393948439fc087a1bf9b278a76eea3
ailyn2020 <ailynlalin2020@gmail.com>
2020-11-14 17:20:22 -0600

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+      public static void main (String [ ] Args) {
+
+      
+      
+      
+        
+   
+       System.out.println();
+    
+      
+        
+       
+        
+}
+}
