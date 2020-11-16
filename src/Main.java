a4a06c97dc8a75e1075b7ab6c95ee2885b3b50f3
ailyn2020 <ailynlalin2020@gmail.com>
2020-11-16 11:41:44 -0600

    commit trabajo finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
+ public class Main {
+      
+   public static void main (String [ ] Args) {
 
-public class Main {
-      public static void main (String [ ] Args) {
-
+      Estudiante estudiante = new Estudiante("Yessenia","Martinez", 16, "Estudiante", "Ramon Molina", "noveno");
+      Deportista deportista=new Deportista("Mario", "Salinas ", 30, "Deportista" , "River");
       
-      
-      
+     
+       System.out.println("Persona 1");
+       System.out.println("Nombre :"+estudiante.getNombre()+"\n"+"Apellido :"+estudiante.getApellido()+"\n"+"Edad  :"+ 
+               estudiante.getEdad()+"\n"+"Dedicacion :"+estudiante.getDedicacion()+"\n"+"Escuela:"+estudiante.getEscuela()+"\n"+"Nivel:"+estudiante.getNivel());
+ 
+       System.out.println(" "); 
+       System.out.println("Persona 2");
         
-   
-       System.out.println();
-    
-      
-        
-       
+       System.out.println("Nombre :"+deportista.getNombre()+"\n"+"Apellido :"+deportista.getApellido()+"\n"+"Edad :"+
+               deportista.getEdad()+"\n"+"Dedicacion :"+deportista.getDedicacion()+"\n"+"Equipo :"+deportista.getEquipo());
         
 }
+    
 }
